7d7a7d2d57c0f19d3f30371c0a56f50b5e87ea9a
Фидаиль <93327801+Shafiddam@users.noreply.github.com>
2022-02-25 10:22:51 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -8,4 +8,3 @@ def test_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     browser.get(link)
     assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), "BUTTON ADD TO BASKET IS NOT PRESENTED..."
-
