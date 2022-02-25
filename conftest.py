127ea32a4041f19a4fd62d4f7a4127a11298c8f3
Фидаиль <93327801+Shafiddam@users.noreply.github.com>
2022-02-25 10:24:17 +0300

    изменил код: убрал условия

--- conftest.py
+++ conftest.py
@@ -4,28 +4,15 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="CHOOSE LANGUAGE: es or ru")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    if language == "es":
-        print("\nSTART test with LANGUAGE as 'ES'...")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        browser = webdriver.Chrome(options=options)
-
-
-    elif language == "ru":
-        print("\nSTART test with LANGUAGE as 'RU'...")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        browser = webdriver.Chrome(options=options)
-
-    else:
-        raise pytest.UsageError("--language should be ES or RU")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nQUIT BROWSER..")
     browser.quit()
